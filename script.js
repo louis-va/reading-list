26a8d5fd25ba3fa4e5ee56b9707ee7d5f079bcc2
Louis Van Aken <vanakenlouis@gmail.com>
2022-01-13 01:08:40 +0100

    Created form to add a book

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let library = [];
+let id = 1;
 
 function Book(title, author, pages, read) {
+    this.id = id++;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -11,28 +13,33 @@ function Book(title, author, pages, read) {
 Book.prototype.isRead = () => (this.read) ? "You read that book" : "You haven't read that book";
 Book.prototype.info = () => `${this.title} by ${this.author}, ${this.pages} pages. ${this.isRead()}.`;
 
-function addBooktoLibrary(book) {
-    library.push(book);
+function displayBook(book) {
+    let bookParameters = ["id", "title", "author", "pages", "read"];
+    let td;
+    let tr = document.createElement('tr');
+    bookParameters.forEach(param => {
+        td = document.createElement('td');
+        td.textContent = book[param];
+        tr.appendChild(td)
+    })
+    document.querySelector('#book_table').appendChild(tr);
 }
 
-function displayBooks() {
-    let bookParameters = ["title", "author", "pages", "read"];
-    let tr, td;
+function addBooktoLibrary(book) {
+    library.push(book);
+    displayBook(book);
+}
 
-    library.forEach(book => {
-        tr = document.createElement('tr')
-        bookParameters.forEach(param => {
-            td = document.createElement('td');
-            td.textContent = book[param];
-            tr.appendChild(td)
-        })
-        document.querySelector('#bookTable').appendChild(tr)
-    })
+document.querySelector("form#add_book").onsubmit = (e) => {
+    e.preventDefault();
+    let name = document.querySelector("input[name='name']").value;
+    let author = document.querySelector("input[name='author']").value;
+    let pages = document.querySelector("input[name='pages']").value;
+    let read = document.querySelector("input[name='read']:checked").value == "y" ? true : false;
+    new Book(name, author, pages, read);
 }
 
 new Book('The Hobbit', 'J.R.R. Tolkien', '295', false);
 new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', '452', true);
 new Book('The Two Towers', 'J.R.R. Tolkien', '632', false);
 new Book('The Return of the King', 'J.R.R. Tolkien', '345', false);
-
-displayBooks();
